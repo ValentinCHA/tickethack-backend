35ef9d0458831bf5bc43549ae12026f775b42fea
Valentin Charpentier <valentincharpentier@hotmail.com>
2022-11-08 21:52:51 +0100

    ajout de la route vers les trajets en back end

---
+++ routes/travels.js
@@ -0,0 +1,32 @@
+var express = require('express');
+//const { response } = require('../app');
+var router = express.Router();
+const moment = require("moment");
+const Travel = require('../models/travel')
+
+
+// GET the trip details using departure / arrival / date ---//
+
+router.get('/search/:departure/:arrival/:date', (req, res) => {
+  const { departure, arrival, date } = req.params;
+  console.log("DATE =>", { $gte: moment(date).startOf('day'), $lte: moment(date).endOf('day') })
+  Travel.find({
+    departure: new RegExp(departure, 'i'),
+    arrival: new RegExp(arrival, 'i'),
+    date: { $gte: moment(date).startOf('day'), $lte: moment(date).endOf('day') },
+  })
+  .then(dbData => {
+    res.json({ dbData })
+  })
+}); 
+
+// TEST / Get all
+router.get('/', (req,res) => {
+  Travel.find({}).then(data => {
+    res.json({data})
+  })
+})
+
+module.exports = router;
+
+
